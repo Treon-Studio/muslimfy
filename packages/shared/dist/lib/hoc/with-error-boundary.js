06abca09416131e494f91f3d0cdc2439de0c81e7
ridhoassuryadi <ulunmuhammadridho@gmail.com>
2025-06-28 08:02:40 +0700

    feat: init turborepo

--- packages/shared/dist/lib/hoc/with-error-boundary.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { ErrorBoundary } from 'react-error-boundary';
-export const withErrorBoundary = (Component, FallbackComponent) => function WithErrorBoundary(props) {
-    return (_jsx(ErrorBoundary, { FallbackComponent: FallbackComponent, children: _jsx(Component, { ...props }) }));
-};
